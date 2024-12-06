10837bc0b1f22828d8f1680575452868136b5ac8
Christopher <chrispatrick96@outlook.com>
2024-12-06 12:38:12 -0500

    Add api.php for api Routing, add api to app config

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\PersonController;
+
+route::get('/projects', [ProjectController::class, 'index']);
+route::get('/projects/{id}', [ProjectController::class, 'show']);
+
+route::get('/people/{name}/projects', [PersonController::class, 'projects']);
