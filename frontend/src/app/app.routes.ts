4b770b27e4f01a2040c652d54f40c161845d339d
Christopher <chrispatrick96@outlook.com>
2024-12-06 12:42:14 -0500

    Added routes for all new project components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { ProjectListComponent } from "./components/project-list/project-list.component";
+import { ProjectSummaryComponent } from "./components/project-summary/project-summary.component";
+import { PersonProjectListComponent } from "./components/person-project-list/person-project-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "projects", component: ProjectListComponent },
+  { path: "projects/:id", component: ProjectSummaryComponent },
+  { path: "people/:name/projects", component: PersonProjectListComponent },
+  { path: "", redirectTo: "/projects", pathMatch: "full" },
+];
