0075c546c697df598e5b3d8a6c383a8b0fc0461a
Christopher <chrispatrick96@outlook.com>
2024-12-06 16:21:31 -0500

    Create Person and Project services

--- frontend/src/app/services/person.service.ts
+++ frontend/src/app/services/person.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PersonService {
+  private API_URL = "http://localhost:8000/api/people";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProjectsByPersonName(name: string): Observable<any> {
+    return this.http.get(`${this.API_URL}/${name}/projects`);
+  }
 }
--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProjectService {
+  private API_URL = "http://localhost:8000/api/projects";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllProjects(): Observable<any> {
+    return this.http.get(`${this.API_URL}`);
+  }
+
+  getProjectById(id: number): Observable<any> {
+    return this.http.get(`${this.API_URL}/${id}`);
+  }
 }
