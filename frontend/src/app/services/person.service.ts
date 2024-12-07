27d2a24305462ef84fa321bdfe8b5a80d69f4da9
Chris Patrick <chrispatrick96@outlook.com>
2024-12-06 21:35:35 -0500

    Added getAllTeamMembers api call function

--- frontend/src/app/services/person.service.ts
+++ frontend/src/app/services/person.service.ts
@@ -1,14 +1,18 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class PersonService {
-  private API_URL = "http://localhost:8000/api/people";
+  private API_URL = 'http://localhost:8000/api/people';
 
   constructor(private http: HttpClient) {}
+
+  getAllTeamMembers(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.API_URL}/team-members`);
+  }
 
   getProjectsByPersonName(name: string): Observable<any> {
     return this.http.get(`${this.API_URL}/${name}/projects`);
