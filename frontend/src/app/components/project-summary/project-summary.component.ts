3ba75347005b16bee547c6f6927c5811a0e65028
Chris Patrick <chrispatrick96@outlook.com>
2024-12-06 21:34:02 -0500

    Completed all functionality for this component

--- frontend/src/app/components/project-summary/project-summary.component.ts
+++ frontend/src/app/components/project-summary/project-summary.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ProjectService } from '../../services/project.service';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-project-summary',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './project-summary.component.html',
-  styleUrl: './project-summary.component.css'
+  styleUrl: './project-summary.component.css',
 })
-export class ProjectSummaryComponent {
+export class ProjectSummaryComponent implements OnInit {
+  project: any;
+  isLoading: boolean = true;
+  displayedColumns: string[] = ['taskName', 'assignedTo', 'estimatedHours'];
 
+  constructor(
+    private route: ActivatedRoute,
+    private projectService: ProjectService
+  ) {}
+
+  ngOnInit(): void {
+    const projectId = this.route.snapshot.params['id'];
+    this.projectService.getProjectById(projectId).subscribe({
+      next: (data) => {
+        this.project = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching project');
+        this.isLoading = false;
+      },
+    });
+  }
 }
