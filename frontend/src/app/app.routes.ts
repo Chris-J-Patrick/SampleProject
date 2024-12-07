e7897f95e9c4c926e6c534d2fe28f8a003652a25
Chris Patrick <chrispatrick96@outlook.com>
2024-12-06 21:31:23 -0500

    Updated various imports

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from "@angular/router";
-import { ProjectListComponent } from "./components/project-list/project-list.component";
-import { ProjectSummaryComponent } from "./components/project-summary/project-summary.component";
-import { PersonProjectListComponent } from "./components/person-project-list/person-project-list.component";
+import { Routes } from '@angular/router';
+import { AllProjectsComponent } from './components/all-projects/all-projects.component';
+import { ProjectSummaryComponent } from './components/project-summary/project-summary.component';
+import { PersonProjectListComponent } from './components/person-project-list/person-project-list.component';
 
 export const routes: Routes = [
-  { path: "projects", component: ProjectListComponent },
-  { path: "projects/:id", component: ProjectSummaryComponent },
-  { path: "people/:name/projects", component: PersonProjectListComponent },
-  { path: "", redirectTo: "/projects", pathMatch: "full" },
+  { path: 'projects', component: AllProjectsComponent },
+  { path: 'projects/:id', component: ProjectSummaryComponent },
+  { path: 'people/:name/projects', component: PersonProjectListComponent },
+  { path: '', redirectTo: '/projects', pathMatch: 'full' },
 ];
