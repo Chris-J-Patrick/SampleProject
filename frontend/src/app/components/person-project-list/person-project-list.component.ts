fde89967467d8c06afbb1c739ce441c154684fd7
Chris Patrick <chrispatrick96@outlook.com>
2024-12-06 21:32:59 -0500

    Renamed file/component to better represent component function

--- frontend/src/app/components/person-project-list/person-project-list.component.ts
+++ frontend/src/app/components/person-project-list/person-project-list.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PersonService } from '../../services/person.service';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-person-project-list',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './person-project-list.component.html',
-  styleUrl: './person-project-list.component.css'
+  styleUrl: './person-project-list.component.css',
 })
-export class PersonProjectListComponent {
+export class PersonProjectListComponent implements OnInit {
+  personName: string = '';
+  projects: any[] = [];
+  isLoading: boolean = true;
+  displayedColumns: string[] = ['projectName', 'members', 'hours', 'actions'];
 
+  constructor(
+    private route: ActivatedRoute,
+    private personService: PersonService
+  ) {}
+
+  ngOnInit(): void {
+    this.personName = this.route.snapshot.params['name'];
+
+    this.personService.getProjectsByPersonName(this.personName).subscribe({
+      next: (data) => {
+        this.projects = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error Fetching Projects for person:', this.personName);
+      },
+    });
+  }
 }
