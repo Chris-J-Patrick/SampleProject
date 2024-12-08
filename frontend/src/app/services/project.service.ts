8a242477709378fe750197bf313f652e8609f47b
Chris Patrick <chrispatrick96@outlook.com>
2024-12-07 23:21:35 -0500

    Added enviroment variables for URL

--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ProjectService {
-  private API_URL = "http://localhost:8000/api/projects";
+  private API_URL = `${environment.apiUrl}/projects`;
 
   constructor(private http: HttpClient) {}
 
