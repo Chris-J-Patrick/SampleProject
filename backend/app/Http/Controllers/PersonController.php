e37dc29423ab28e7bf820af6c16fab0719017264
Chris Patrick <chrispatrick96@outlook.com>
2024-12-09 04:31:53 -0500

    Added try/catch to controller functions Improved code formatting

--- backend/app/Http/Controllers/PersonController.php
+++ backend/app/Http/Controllers/PersonController.php
@@ -8,38 +8,47 @@ use App\Models\Person;
 
 class PersonController extends Controller 
 {
-
     // Returns all team members 
     public function index() {
-        $teamMembers = Person::all()->pluck('name')->map(function ($name){
-            return ['name' => $name];
+        try {
+            $teamMembers = Person::all()->pluck('name')->map(function ($name) {
+                return ['name' => $name];
         });
+
         return response()->json($teamMembers);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Failed to get all team members'], 500);
+        }
     }
 
     // Returns all Projects belonging to a Person, using a Person's name
-    public function projects($name) 
-    {
+    public function projects($name) {
+        try {
+            $query = Project::with('tasks.person');
 
-        $query = Project::with('tasks.person');
-
-        $query->whereHas('tasks.person', function ($q) use ($name) {
-            $q->where('name', $name);
-        });
-
-        $projects = $query->get()->map(function ($project) use ($name) {
-            $members = $project->tasks->pluck('person.name')->unique()->values()->map(function($member) {
-                return ['name' => $member];
+            $query->whereHas('tasks.person', function ($q) use ($name) {
+                $q->where('name', $name);
             });
-            $totalHours = $project->tasks->sum('estimated_hours');
-            return [
-                'project_name' => $project->name,
-                'members' => $members,
-                'estimated_hours' => $totalHours, 
-                'id' => $project->id
-            ];
-        });
-        return response()->json($projects);
+
+            $projects = $query->get()->map(function ($project) use ($name) {
+                $members = $project->tasks->pluck('person.name')->unique()->values()->map(function($member) {
+                    return ['name' => $member];
+                });
+                $totalHours = $project->tasks->sum('estimated_hours');
+
+                return [
+                    'project_name' => $project->name,
+                    'members' => $members,
+                    'estimated_hours' => $totalHours, 
+                    'id' => $project->id
+                ];
+            });
+
+            return response()->json($projects);
+
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Failed to get Projects belonging to name'], 500);
+        }
     }
-    
 }
--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -5,48 +5,60 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
-class ProjectController extends Controller 
+class ProjectController extends Controller
 {
     // Returns all projects with all project info
-    public function index() 
+    public function index()
     {
-        $projects = Project::with('tasks.person')->get()->map(function ($project) {
-            $members = $project->tasks->pluck('person.name')->unique()->values()->map(function ($member) {
-                return ['name' => $member];
-            } );
-            $totalHours = $project->tasks->sum('estimated_hours');
-            return [
-                'project_name' => $project->name,
-                'members' => $members,
-                'estimated_hours' => $totalHours,
-                'id' => $project->id
-            ];
-        });
-        return response()->json($projects);
+        try {
+            $projects = Project::with('tasks.person')->get()->map(function ($project) {
+                $members = $project->tasks->pluck('person.name')->unique()->values()->map(function ($member) {
+                    return ['name' => $member];
+                });
+                $totalHours = $project->tasks->sum('estimated_hours');
+                return [
+                    'project_name' => $project->name,
+                    'members' => $members,
+                    'estimated_hours' => $totalHours,
+                    'id' => $project->id
+                ];
+            });
+
+            return response()->json($projects);
+            
+        } catch (Exception $e) {
+            return response()->json(['error' => "Failed to fetch Projects"], 500);
+        }
     }
 
-    public function show($id) 
-    {
-        $project = Project::with('tasks.person')->findOrFail($id);
-        $totalHours = $project->tasks->sum('estimated_hours');
-        $members = $project->tasks->pluck('person.name')->unique()->values()->map(function ($person){
-            return [
-                'name' => $person,
-            ];
-        });
+    public function show($id)
+    {   
+        try {
+            $project = Project::with('tasks.person')->findOrFail($id);
+            $totalHours = $project->tasks->sum('estimated_hours');
+            $members = $project->tasks->pluck('person.name')->unique()->values()->map(function ($person) {
+                return [
+                    'name' => $person,
+                ];
+            });
 
-        $taskData = $project->tasks->map(function($task) {
-            return [
-                'task_name' => $task->name,
-                'assigned_to' => $task->person->name,
-                'estimated_hours' => $task->estimated_hours
-            ];
-        });
-        return response()->json([
-            'project_name' => $project->name,
-            'total_hours' => $totalHours,
-            'members' => $members,
-            'tasks' => $taskData
-        ]);
+            $taskData = $project->tasks->map(function ($task) {
+                return [
+                    'task_name' => $task->name,
+                    'assigned_to' => $task->person->name,
+                    'estimated_hours' => $task->estimated_hours
+                ];
+            });
+            
+            return response()->json([
+                'project_name' => $project->name,
+                'total_hours' => $totalHours,
+                'members' => $members,
+                'tasks' => $taskData
+            ]);
+
+        } catch(Exception $e) {
+            return response()->json(['error' => 'Failed to fetch project by ID'], 500);
+        }
     }
 }
